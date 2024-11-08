1a19f9e78212956116bec50dea532f68b7af2f27
anamitradebnath <anamitra.debnath@gmail.com>
2024-11-08 14:14:44 +0530

    Removed unused code
    Removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Header from "./Components/Header"
 import Section1 from "./Components/Section1"
 import Section2 from "./Components/Section2"
 import Section3 from "./Components/Section3"
@@ -6,8 +5,7 @@ import HorizontalLine from "./Components/HorizontalLine"
 function App() {
 
   return (
-    <>
-      {/* <Header /> */}
+    <>      
       <Section1 />
       <HorizontalLine />
       <Section2 />
--- src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-
-    return (
-        <div>
-            <h1>GENERATE STORIES WITH IMAGES</h1>
-        </div>
-    )
-}
-export default Header
